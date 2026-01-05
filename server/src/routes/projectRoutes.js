0f6f90e4edae5b76f00a01cad8e8f572047d6781
Iyyanarsuriya <suriyaiyyanar10@gmail.com>
2026-01-05 10:59:20 +0530

    feat: Implement client build configuration, project API routes, and an authenticated Axios client.

--- server/src/routes/projectRoutes.js
+++ server/src/routes/projectRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const projectController = require('../controllers/projectController');
-const { authenticateToken } = require('../middlewares/authMiddleware');
+const authenticateToken = require('../middlewares/authMiddleware');
 
 router.use(authenticateToken);
 
